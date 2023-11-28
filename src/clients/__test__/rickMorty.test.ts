01f3f7459e14d65a4714ce55a20cc6b84399a6e4
macwdo <danilo.macedofernandes@hotmail.com>
2023-11-28 10:59:48 -0300

    adding more tests and mocking :hammer:

--- src/clients/__test__/rickMorty.test.ts
+++ src/clients/__test__/rickMorty.test.ts
@@ -3,20 +3,48 @@ import { RickMorty } from "@src/clients/rickMorty";
 
 import characterFixture from "@test/fixtures/character.json";
 import characterLocationFixture from "@test/fixtures/character_location.json";
+import locationFixture from "@test/fixtures/location.json";
+import locationResidentsFixture from "@test/fixtures/location_residents.json";
+
 
 jest.mock("axios");
 
 describe("Rick Morty Client", () => {
 
     const mockedAxios = axios as jest.Mocked<typeof axios>;
-    it("should return location from character", async () => {
+    it("should return location fields from character request", async () => {
 
-        const characterId = 1;
+        const anyCharacterId = 1;
         mockedAxios.get.mockResolvedValue({data: characterFixture});
 
         const rickMorty = new RickMorty(mockedAxios);
-        const response = await rickMorty.findCharacterLocation(characterId);
+        const locationFields = await rickMorty.getCharacterLocationFields(anyCharacterId);
 
-        expect(response).toEqual(characterLocationFixture);
+        expect(locationFields).toEqual(characterLocationFixture);
     });
+
+    it("should return location detail from character request", async () => {
+
+        const anyCharacterId = 1;
+        mockedAxios.get.mockResolvedValueOnce({data: characterFixture});
+        mockedAxios.get.mockResolvedValueOnce({data: locationFixture});
+
+        const rickMorty = new RickMorty(mockedAxios);
+        const location = await rickMorty.getCharacterLocation(anyCharacterId);
+
+        expect(location).toEqual(locationFixture);
+
+    });
+
+    it("should return location residents by character id", async () => {
+        const anyCharacterId = 1;
+        mockedAxios.get.mockResolvedValueOnce({data: characterFixture});
+        mockedAxios.get.mockResolvedValueOnce({data: locationFixture});
+
+        const rickMorty = new RickMorty(mockedAxios);
+        const residents = await rickMorty.getLocationResidentByCharacterId(anyCharacterId);
+
+        expect(residents).toEqual(locationResidentsFixture);
+    });
+
 });
--- src/clients/rickMorty.ts
+++ src/clients/rickMorty.ts
@@ -42,12 +42,27 @@ export class RickMorty {
 
     readonly rickMortyApiUrl: string = "https://rickandmortyapi.com/api";
 
-    constructor(protected request: AxiosStatic) {}
-    public async findCharacterLocation(characterId: number): Promise<CharacterLocation> {
+    constructor(protected request: AxiosStatic) {
+
+    }
+    public async getCharacterLocationFields(characterId: number): Promise<CharacterLocation> {
         const response = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
         return {
             name: response.data.location.name,
             url: response.data.location.url,
         };
     }
+
+    public async getCharacterLocation(characterId: number): Promise<Location> {
+        const characterResponse = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
+        const locationResponse = await this.request.get<Location>(characterResponse.data.url);
+        return locationResponse.data;
+    }
+
+    public async getLocationResidentByCharacterId(characterId: number): Promise<string[]>{
+        const characterResponse = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
+        const locationResponse = await this.request.get<Location>(characterResponse.data.url);
+        return locationResponse.data.residents;
+    }
+
 }
