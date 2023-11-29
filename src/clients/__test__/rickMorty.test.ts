e6ed62fb16d095e0ba30903bce56a0102189d48a
macwdo <danilo.macedofernandes@hotmail.com>
2023-11-28 21:16:50 -0300

    :sparkles: try catch handle and tests with jest

--- src/clients/__test__/rickMorty.test.ts
+++ src/clients/__test__/rickMorty.test.ts
@@ -6,45 +6,91 @@ import characterLocationFixture from "@test/fixtures/character_location.json";
 import locationFixture from "@test/fixtures/location.json";
 import locationResidentsFixture from "@test/fixtures/location_residents.json";
 
-
 jest.mock("axios");
 
 describe("Rick Morty Client", () => {
-
     const mockedAxios = axios as jest.Mocked<typeof axios>;
     it("should return location fields from character request", async () => {
-
         const anyCharacterId = 1;
-        mockedAxios.get.mockResolvedValue({data: characterFixture});
+        mockedAxios.get.mockResolvedValue({ data: characterFixture });
 
-        const rickMorty = new RickMorty(mockedAxios);
-        const locationFields = await rickMorty.getCharacterLocationFields(anyCharacterId);
+        const rickMortyClient = new RickMorty(mockedAxios);
+        const locationFields =
+            await rickMortyClient.getCharacterLocationFields(anyCharacterId);
 
         expect(locationFields).toEqual(characterLocationFixture);
     });
 
     it("should return location detail from character request", async () => {
-
         const anyCharacterId = 1;
-        mockedAxios.get.mockResolvedValueOnce({data: characterFixture});
-        mockedAxios.get.mockResolvedValueOnce({data: locationFixture});
+        mockedAxios.get.mockResolvedValueOnce({ data: characterFixture });
+        mockedAxios.get.mockResolvedValueOnce({ data: locationFixture });
 
-        const rickMorty = new RickMorty(mockedAxios);
-        const location = await rickMorty.getCharacterLocation(anyCharacterId);
+        const rickMortyClient = new RickMorty(mockedAxios);
+        const location =
+            await rickMortyClient.getCharacterLocation(anyCharacterId);
 
         expect(location).toEqual(locationFixture);
-
     });
 
     it("should return location residents by character id", async () => {
         const anyCharacterId = 1;
-        mockedAxios.get.mockResolvedValueOnce({data: characterFixture});
-        mockedAxios.get.mockResolvedValueOnce({data: locationFixture});
+        mockedAxios.get.mockResolvedValueOnce({ data: characterFixture });
+        mockedAxios.get.mockResolvedValueOnce({ data: locationFixture });
 
-        const rickMorty = new RickMorty(mockedAxios);
-        const residents = await rickMorty.getLocationResidentByCharacterId(anyCharacterId);
+        const rickMortyClient = new RickMorty(mockedAxios);
+        const residents =
+            await rickMortyClient.getLocationResidentsByCharacterId(
+                anyCharacterId,
+            );
 
         expect(residents).toEqual(locationResidentsFixture);
     });
 
+    it("should get a generic error from RickMorty service when the request fail before reaching th service", async () => {
+        const anyCharacterId = 1;
+
+        mockedAxios.get.mockRejectedValue({ message: "Network Error" });
+
+        const rickMortyClient = new RickMorty(mockedAxios);
+
+        await expect(
+            rickMortyClient.getCharacterLocation(anyCharacterId),
+        ).rejects.toThrow(
+            "Unexpected error when trying to communicate to RickMorty service: Network Error",
+        );
+
+        await expect(
+            rickMortyClient.getCharacterLocationFields(anyCharacterId),
+        ).rejects.toThrow(
+            "Unexpected error when trying to communicate to RickMorty service: Network Error",
+        );
+
+        await expect(
+            rickMortyClient.getLocationResidentsByCharacterId(anyCharacterId),
+        ).rejects.toThrow(
+            "Unexpected error when trying to communicate to RickMorty service: Network Error",
+        );
+    });
+
+    it("should get a 'RickMortyResponseError' when the RickMorty Service responds with error", async () => {
+        const anyCharacterId = 1;
+
+        mockedAxios.get.mockRejectedValue({
+            response: {
+                status: 400,
+                data: {
+                    errors: ["Some bad request"],
+                },
+            },
+        });
+
+        const rickMortyService = new RickMorty(mockedAxios);
+
+        await expect(
+            rickMortyService.getCharacterLocation(anyCharacterId),
+        ).rejects.toThrow(
+            "Unexpected error returned by the RickMorty service: Error {\"errors\":[\"Some bad request\"]} Code: 400",
+        );
+    });
 });
--- src/clients/rickMorty.ts
+++ src/clients/rickMorty.ts
@@ -1,5 +1,5 @@
-import { AxiosStatic } from "axios";
-
+import { InternalError } from "@src/utils/internal-error";
+import { AxiosError, AxiosStatic } from "axios";
 
 export interface Location {
     readonly id: number;
@@ -36,33 +36,99 @@ export interface Character {
     readonly created: string;
 }
 
+export class RickMortyResponseError extends InternalError {
+    constructor(message: string) {
+        const internalMessage =
+            "Unexpected error returned by the RickMorty service";
+        super(`${internalMessage}: ${message}`, 400);
+    }
+}
 
+export class ClientRequestError extends InternalError {
+    constructor(message: string) {
+        const internalMessage =
+            "Unexpected error when trying to communicate to RickMorty service";
+        super(`${internalMessage}: ${message}`);
+    }
+}
 
 export class RickMorty {
-
     readonly rickMortyApiUrl: string = "https://rickandmortyapi.com/api";
 
-    constructor(protected request: AxiosStatic) {
-
-    }
-    public async getCharacterLocationFields(characterId: number): Promise<CharacterLocation> {
-        const response = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
-        return {
-            name: response.data.location.name,
-            url: response.data.location.url,
-        };
+    constructor(protected request: AxiosStatic) {}
+    public async getCharacterLocationFields(
+        characterId: number,
+    ): Promise<CharacterLocation> {
+        try {
+            const response = await this.request.get<Character>(
+                `${this.rickMortyApiUrl}/character/${characterId}`,
+            );
+            return {
+                name: response.data.location.name,
+                url: response.data.location.url,
+            };
+        } catch (e) {
+            if (
+                (e as AxiosError).response &&
+                (e as AxiosError).response?.data
+            ) {
+                throw new RickMortyResponseError(
+                    `Error ${JSON.stringify(
+                        (e as AxiosError).response?.data,
+                    )} Code: ${(e as AxiosError).response?.status}`,
+                );
+            }
+            throw new ClientRequestError((e as Error).message);
+        }
     }
 
     public async getCharacterLocation(characterId: number): Promise<Location> {
-        const characterResponse = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
-        const locationResponse = await this.request.get<Location>(characterResponse.data.url);
-        return locationResponse.data;
+        try {
+            const characterResponse = await this.request.get<Character>(
+                `${this.rickMortyApiUrl}/character/${characterId}`,
+            );
+            const locationResponse = await this.request.get<Location>(
+                characterResponse.data.url,
+            );
+            return locationResponse.data;
+        } catch (e) {
+            if (
+                (e as AxiosError).response &&
+                (e as AxiosError).response?.data
+            ) {
+                throw new RickMortyResponseError(
+                    `Error ${JSON.stringify(
+                        (e as AxiosError).response?.data,
+                    )} Code: ${(e as AxiosError).response?.status}`,
+                );
+            }
+            throw new ClientRequestError((e as Error).message);
+        }
     }
 
-    public async getLocationResidentByCharacterId(characterId: number): Promise<string[]>{
-        const characterResponse = await this.request.get<Character>(`${this.rickMortyApiUrl}/character/${characterId}`);
-        const locationResponse = await this.request.get<Location>(characterResponse.data.url);
-        return locationResponse.data.residents;
+    public async getLocationResidentsByCharacterId(
+        characterId: number,
+    ): Promise<string[]> {
+        try {
+            const characterResponse = await this.request.get<Character>(
+                `${this.rickMortyApiUrl}/character/${characterId}`,
+            );
+            const locationResponse = await this.request.get<Location>(
+                characterResponse.data.url,
+            );
+            return locationResponse.data.residents;
+        } catch (e) {
+            if (
+                (e as AxiosError).response &&
+                (e as AxiosError).response?.data
+            ) {
+                throw new RickMortyResponseError(
+                    `Error ${JSON.stringify(
+                        (e as AxiosError).response?.data,
+                    )} Code: ${(e as AxiosError).response?.status}`,
+                );
+            }
+            throw new ClientRequestError((e as Error).message);
+        }
     }
-
 }
